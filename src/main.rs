55228c1dc822b1c2c97eaf530d9e44f3088fcaee
Michael Pankov <work@michaelpankov.com>
2019-11-20 11:02:51 +0300

    Forget a configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
     use librpm::Index;
 
-    librpm::config::read_file(None).unwrap();
-
     let mut matches = Index::Name.find("rpm-devel");
     let package = matches.next();
     println!("{:?}", if package.is_some() {
